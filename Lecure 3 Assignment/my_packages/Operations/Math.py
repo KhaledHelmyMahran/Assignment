ac14e774c077987c7b8d4f70000800d4c01afab3
KhaledHelmy <khaledhelmy774@gmail.com>
2021-02-11 22:06:23 -0800

    update

--- Lecure 3 Assignment/my_packages/Operations/Math.py
+++
@@ -1,5 +0,0 @@
-def get_average(lst):
-	summ = 0
-	for el in lst:
-		summ += el
-	return summ / len(lst)
--- Lecure 3 Assignment/my_packages/Transformers/StringTransformer.py
+++
@@ -1,6 +0,0 @@
-def reverse_word(word):
-	return word[::-1]
-
-
-def capitalize_word(word):
-	return word.capitalize()
